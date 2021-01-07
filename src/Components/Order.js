567c99bb748f26817cf4bd1dd07c5961ffbc9e29
Khai Nguyen <khainl1110@gmail.com>
2021-01-06 18:44:32 -0800

    Improving the site

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,15 +1,15 @@
-import React, {useState, useContext} from "react"
+import React, {useState, useContext, useEffect} from "react"
 
 import MyContext from "../Contexts/MyContext"
 export default function Order(props){
     var [cart, setCart] = useContext(MyContext)
-    var [tempQuantity, setTempQuantity] = useState(0)
+    var [tempQuantity, setTempQuantity] = useState(1)
+
     function addItem(){
         var added = false
         cart.map(item => {
             if(item.name===props.order.name) {
                 var newCart = [...cart]
-                if(tempQuantity===0) setTempQuantity(1)
                 newCart[newCart.indexOf(item)].quantity+=parseInt(tempQuantity)
                 setCart(newCart)
                 alert("Added!")
@@ -18,8 +18,7 @@ export default function Order(props){
         })
         if(!added){
             var newCart = [...cart]
-            setTempQuantity(1)
-            newCart.push({name: props.order.name, quantity: 1})
+            newCart.push({name: props.order.name, quantity: tempQuantity})
             setCart(newCart)
             alert("Added new item!")
         }
@@ -33,8 +32,7 @@ export default function Order(props){
             <img src={props.order.href} alt={props.order.name} width="300" height="250"/>
             <p>{props.order.name}</p>
             {
-                (tempQuantity===0) ? <button onClick={addItem}>Add to cart </button>:
-                <div>
+                <div className = "horizontal-flex-container">
                     <input type="Number" value={tempQuantity} onChange={handleInputChange}/>
                     <button onClick = {addItem}> Add </button>
                 </div>
